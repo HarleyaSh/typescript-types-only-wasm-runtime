0047b9a86aeea00709fce184ab98f8e54601f44e
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2025-03-07 15:24:28 -0500

    reduced examples to show Anders

--- packages/playground/evaluate/start.ts
+++ packages/playground/evaluate/start.ts
@@ -1,3 +1,3 @@
-import type { funcs } from "../doom/doom";
-import type { entry } from "../doom/doom";
-export type NextResult = entry<[], true, 0>;
+import type { funcs } from "../../conformance-tests/from-c/conway";
+import type { entry } from "../../conformance-tests/from-c/conway";
+export type NextResult = entry<[0], true, 0>;
